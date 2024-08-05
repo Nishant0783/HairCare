7adca210ab8d4446b3097d20205ce7f42f9e028f
Nishant Paliwal <paliwalnishant2003@gmail.com>
2024-08-05 22:24:08 +0530

    add responsive hero left

--- frontend/src/components/Button/Button.jsx
+++ frontend/src/components/Button/Button.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Button = ({ bgColor, content, onClick, contentColor, extraClass, ...props }) => {
     return (
         <button
-            className={`bg-${bgColor} rounded-lg text-${contentColor} font-content sm:text-[1.5rem] font-semibold ${extraClass}`}
+            className={`${bgColor} rounded-lg ${contentColor} font-content sm:text-[1.5rem] font-semibold ${extraClass}`}
             {...props}
         >
             {content}
--- frontend/src/components/Hero/HeroLeft.jsx
+++ frontend/src/components/Hero/HeroLeft.jsx
@@ -5,22 +5,22 @@ const HeroLeft = () => {
     return (
         <div className='flex flex-col sm:py-[50px] gap-y-[50px]'>
             <div className='flex flex-col'>
-                <div className='font-title 2xl:text-[6rem] xl:text-[5rem] font-semibold text-content'>
+                <div className='font-title 2xl:text-[6rem] xl:text-[5rem] lg:text-[4rem] md:text-[3rem] font-semibold text-content'>
                     <p className='leading-tight'>Need Health Consultation?</p>
                 </div>
-                <div className='font-title 2xl:text-[5rem] xl:text-[4rem] font-semibold text-content'>
+                <div className='font-title 2xl:text-[5rem] xl:text-[4rem] lg:text-[3rem] md:text-[2rem] font-semibold text-content'>
                     <p>Now on Your Hands.</p>
                 </div>
-                <div className='font-title sm:text-[1.5rem]  text-content mt-[-10px] w-[80%]'>
+                <div className='font-title text-[0.8rem] md:text-[1rem] xl:text-[1.5rem] text-content mt-[-10px] md:w-[80%]'>
                     <p>Clinical excellence must be priority for any health care service provider.</p>
                 </div>
             </div>
             <div>
                 <Button 
-                    bgColor={'btn'}
+                    bgColor={'bg-btn'}
                     content={'Take Online Test'}
-                    contentColor={'btn-text'}
-                    extraClass={'px-[80px] py-[10px]'}
+                    contentColor={'text-btn-text'}
+                    extraClass={'px-[20px] py-[10px] lg:px-[80px] md:px-[50px] md:text-[1.2rem]'}
                 />
             </div>
         </div>
