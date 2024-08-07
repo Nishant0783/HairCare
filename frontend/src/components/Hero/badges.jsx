92e19492977d6d5b02a4b7d545957e469c1b690a
Nishant Paliwal <paliwalnishant2003@gmail.com>
2024-08-07 14:37:12 +0530

    add responsive for test

--- frontend/src/components/Hero/badges.jsx
+++ frontend/src/components/Hero/badges.jsx
@@ -14,13 +14,13 @@ const badges = [
         id: uuidv4(),
         icon: <BiSolidNotepad className='w-[20px] h-[20px] text-orange-600'/>,
         text: 'Online Test',
-        className: 'absolute top-10 left-10 lg:top-[600px] lg:-left-20 z-10'
+        className: 'absolute top-10 left-10 lg:top-[600px] lg:-left-20 md:top-[400px] top-[400px] left-[10px] z-10'
     },
     {
         id: uuidv4(),
         icon: <FaCheckCircle className='w-[20px] h-[20px] text-green-600'/>,
         text: 'Trusted Results',
-        className: 'absolute top-10 left-10 xl:top-[350px] xl:left-[400px] lg:top-[350px] lg:left-[350px] z-10'
+        className: 'absolute top-10 left-10 xl:top-[350px] xl:left-[400px] lg:top-[350px] lg:left-[350px] md:top-[400px] md:left-[500px] top-[300px] left-[150px] z-10'
     },
 ]
 
