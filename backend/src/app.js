87579f278db4b981215a5f1f371a20aa62c0b8eb
Nishant Paliwal <paliwalnishant2003@gmail.com>
2024-08-17 22:41:44 +0530

    backend complete

---
+++ backend/src/app.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
+
+// Express accepting JSON
+app.use(express.json({
+    limit: '16kb' // Size of data allowed on server
+}))
+
+// configuring express for urlencodings
+app.use(express.urlencoded({extended: true,limit: '16kb'}))
+
+// configuring static files
+app.use(express.static('public'))
+
+// Router imports
+import reportRouter from './routes/report.routes.js'
+
+// Report router
+app.use("/api/v1/report", reportRouter)
+
+export { app };
