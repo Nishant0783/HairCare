d60602c40c9a486f6ae7f1f7de135f9e0d35aab7
Nishant Paliwal <141571596+Nishant0783@users.noreply.github.com>
2024-08-19 21:28:30 +0530

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,19 +3,19 @@ import cors from 'cors';
 
 const app = express();
 
-const allowedOrigins = process.env.CORS_ORIGINS.split(',');
+// const allowedOrigins = process.env.CORS_ORIGINS.split(',');
 
-const corsOptions = {
-    origin: function (origin, callback) {
-        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
-};
+// const corsOptions = {
+//     origin: function (origin, callback) {
+//         if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+//             callback(null, true);
+//         } else {
+//             callback(new Error('Not allowed by CORS'));
+//         }
+//     },
+// };
 
-app.use(cors(corsOptions));
+app.use(cors());
 
 // Express accepting JSON
 app.use(express.json({
